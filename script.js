8c912b879c7de4f64838a9a3cf13e10034e061e2
Nydra <contacto@nydra.org>
2025-12-03 10:44:42 -0700

    redirect

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ document.addEventListener('mousemove', (e) => {
     const grid = document.querySelector('.background-grid');
     const x = e.clientX / window.innerWidth;
     const y = e.clientY / window.innerHeight;
-    
+
     grid.style.transform = `translate(-${x * 10}px, -${y * 10}px)`;
 });
