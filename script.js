1730a0a9f9042b4b0d1bf919698e81be175597dc
Nydra <contacto@nydra.org>
2025-12-02 22:15:03 -0700

    Initial commit: Deploying static Nydra landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('Nydra - Ciberseguridad para MIPyMEs');
+
+// Add a subtle parallax effect to the background grid if desired
+document.addEventListener('mousemove', (e) => {
+    const grid = document.querySelector('.background-grid');
+    const x = e.clientX / window.innerWidth;
+    const y = e.clientY / window.innerHeight;
+    
+    grid.style.transform = `translate(-${x * 10}px, -${y * 10}px)`;
+});
